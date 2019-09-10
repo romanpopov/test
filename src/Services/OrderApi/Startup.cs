4882e164a86c1576f1120da999e71c44a157f753
romanpopov <popovro@gmail.com>
2019-09-10 22:45:56 +0300

    first commit'

---
+++ src/Services/OrderApi/Startup.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using ShoesOnContainers.Services.OrderApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using MySql.Data.MySqlClient;
+using System.Threading;
+using Swashbuckle.AspNetCore.Swagger;
+using ShoesOnContainers.Services.OrderApi.Infrastructure.Filters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace ShoesOnContainers.Services.OrderApi
+{
+    public class Startup
+    {
+        private readonly string _connectionString;
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            //_connectionString = $@"Server={Configuration["MYSQL_SERVER_NAME"]};
+            //                       Database={Configuration["MYSQL_DATABASE"]};
+            //                       Uid={Configuration["MYSQL_USER"]};
+            //                       Pwd={Configuration["MYSQL_PASSWORD"]}";
+            _connectionString = Configuration["ConnectionString"];
+        }
+
+
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+
+            services.AddMvcCore(
+                 options => options.Filters.Add(typeof(HttpGlobalExceptionFilter))
+                 )
+                  .AddJsonFormatters(
+                   Options =>
+                   {
+                       Options.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                       Options.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                   }
+                )
+                  .AddApiExplorer();
+
+            services.Configure<OrderSettings>(Configuration);
+
+            ConfigureAuthService(services);
+            //.AddJsonOptions(options =>
+            //{
+            //    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            //});
+
+            //WaitForDBInit(_connectionString);
+
+            var hostname = Environment.GetEnvironmentVariable("SQLSERVER_HOST") ?? "mssqlserver";
+            var password = Environment.GetEnvironmentVariable("SA_PASSWORD") ?? "MyProduct!123";
+            var database = Environment.GetEnvironmentVariable("DATABASE") ?? "OrdersDb";
+
+            var connectionString = $"Server={hostname};Database={database};User ID=sa;Password={password};";
+
+
+            services.AddDbContext<OrdersContext>(options =>
+            {
+                options.UseSqlServer(connectionString,
+                                     sqlServerOptionsAction: sqlOptions =>
+                                     {
+                                         sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                                         //Configuring Connection Resiliency: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency 
+                                         sqlOptions.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                                     });
+
+                // Changing default behavior when client evaluation occurs to throw. 
+                // Default in EF Core would be to log a warning when client evaluation is performed.
+                options.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+                //Check Client vs. Server evaluation: https://docs.microsoft.com/en-us/ef/core/querying/client-eval
+            });
+
+
+            //var host = Configuration["Server"];
+            //var port = "3406";
+            //var password = "order123";
+
+            //// var conString = envs["ConnectionString"] ?? Configuration["ConnectionString"];
+
+            //services.AddDbContext<OrdersContext>(options =>
+            //   options.UseMySql($"server={host};userid=root;pwd={password};port={port};database=OrderDb"));
+
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Ordering HTTP API",
+                    Version = "v1",
+                    Description = "The Ordering Service HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+                options.AddSecurityDefinition("oauth2", new OAuth2Scheme
+                {
+                    Type = "oauth2",
+                    Flow = "implicit",
+                    AuthorizationUrl = $"{Configuration.GetValue<string>("IdentityUrl")}/connect/authorize",
+                    TokenUrl = $"{Configuration.GetValue<string>("IdentityUrl")}/connect/token",
+                    Scopes = new Dictionary<string, string>()
+                    {
+                        { "order", "Order Api" }
+                    }
+
+                });
+                options.OperationFilter<AuthorizeCheckOperationFilter>();
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+        }
+        private void ConfigureAuthService(IServiceCollection services)
+        {
+            // prevent from mapping "sub" claim to nameidentifier.
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            var identityUrl = Configuration.GetValue<string>("IdentityUrl");
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = identityUrl;
+                options.RequireHttpsMetadata = false;
+                options.Audience = "order";
+
+            });
+        }
+        private void WaitForDBInit(string connectionString)
+        {
+            var connection = new MySqlConnection(connectionString);
+            int retries = 1;
+            while (retries < 7)
+            {
+                try
+                {
+                    Console.WriteLine("Connecting to db. Trial: {0}", retries);
+                    connection.Open();
+                    connection.Close();
+                    break;
+                }
+                catch (MySqlException)
+                {
+                    Thread.Sleep((int)Math.Pow(2, retries) * 1000);
+                    retries++;
+                }
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, OrdersContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            var pathBase = Configuration["PATH_BASE"];
+            if (!string.IsNullOrEmpty(pathBase))
+            {
+                app.UsePathBase(pathBase);
+            }
+
+            context.Database.Migrate();
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
+            // app.UseMvc();
+            //  app.UseMvcWithDefaultRoute();
+            app.UseSwagger()
+              .UseSwaggerUI(c =>
+              {
+                  c.SwaggerEndpoint($"{ (!string.IsNullOrEmpty(pathBase) ? pathBase : string.Empty) }/swagger/v1/swagger.json", "OrderApi V1");
+                  c.OAuthClientId("orderswaggerui");
+                  //c.OAuthClientSecret("test-secret");
+                  //c.OAuthRealm("test-realm");
+                  c.OAuthAppName("Ordering Swagger UI");
+                  c.OAuthScopeSeparator(" ");
+                  c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+
+                  //c.ConfigureOAuth2("orderswaggerui", "", "", "Ordering Swagger UI");
+              });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}");
+            });
+
+        }
+    }
+}
